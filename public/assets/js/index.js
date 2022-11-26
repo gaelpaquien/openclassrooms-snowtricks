d5ec78c7a2f0146d9f1ef3691c174a003689ab75
Gael <gael.paquien.contact@gmail.com>
2022-11-26 08:25:12 +0100

    feat:(templates-#9): Layout of the homepage for large screen

--- public/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-// Management of the hero's image height according to the screen height
-$(".header").css({ height: ($(window).height() + "px") });
-
-$(window).on("resize", function() {
-  $(".header").css({ height: $(window).height() + "px" });
-});
