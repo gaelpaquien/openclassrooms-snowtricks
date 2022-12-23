a1b09005d7e7fd3018d4e89bbf6cd372a9b8b07e
Gaël Paquien <contact@gaelpaquien.com>
2022-12-23 19:48:11 +0100

    feat:(forms): Update Tricks Forms

--- src/Form/Tricks/TricksVideosFormType.php
+++ src/Form/Tricks/TricksVideosFormType.php
@@ -4,7 +4,7 @@ namespace App\Form\Tricks;
 
 use App\Entity\TricksVideos;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +13,7 @@ class TricksVideosFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('video', FileType::class, [
+        ->add('video', UrlType::class, [
             'attr' => [
                 'class' => 'form-control'
             ],
