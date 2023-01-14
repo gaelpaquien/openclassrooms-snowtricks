9058264a4412ab4ed5f558bade10406ed0db8e3e
Gaël Paquien <contact@gaelpaquien.com>
2023-01-14 01:25:29 +0100

    feat:(pagination-#23): Setting up pagination system for comments

--- src/Repository/TricksRepository.php
+++ src/Repository/TricksRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Tricks;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
