861d6d67f019e31ab831f5e6d357c65b79837962
Gael <gael.paquien.contact@gmail.com>
2023-02-21 01:30:52 +0100

    Update template to prepare modal confirm when trick are deleted

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,3 +3,15 @@ function scrollToTricks() {
     const element = document.getElementById("tricks");
     element.scrollIntoView({ behavior: "smooth" });
 }
+
+// WIP: Delete trick confirmation modal
+document.addEventListener("DOMContentLoaded", function(event) {
+    const deleteButtons = document.querySelectorAll('.card-btn-delete');
+    console.log(deleteButtons);
+    deleteButtons.forEach((deleteButton) => {
+        this.addEventListener('click', (event) => {
+            event.preventDefault();
+            console.log('test');
+        }
+    )});
+  });
