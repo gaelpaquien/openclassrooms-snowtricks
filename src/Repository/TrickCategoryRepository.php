36f269b6c830ab15d8358d88f4ce6380494ec42c
Gael <gael.paquien.contact@gmail.com>
2023-03-01 04:02:09 +0100

    fix:(Codacy): Fix Codacy

--- src/Repository/TrickCategoryRepository.php
+++ src/Repository/TrickCategoryRepository.php
@@ -38,29 +38,4 @@ class TrickCategoryRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return TrickCategory[] Returns an array of TrickCategory objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?TrickCategory
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
