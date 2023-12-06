b8af2266e11baa3f56586cb03252759a2d6c3cd6
Gaël Paquien <contact@gaelpaquien.com>
2023-12-06 22:36:55 +0100

    Project update to prepare for deployment

--- public/assets/js/popup.js
+++ public/assets/js/popup.js
@@ -4,32 +4,16 @@ const closeButton = document.getElementById('close-popup');
 const triggerButton = document.getElementById('popup-trigger');
 const navigation = document.getElementById('navigation-sticky');
 
-// For the first time, create the sessionStorage item
-if (sessionStorage.getItem('popupShown') === null) {
-    sessionStorage.setItem('popupShown', 'false');
-}
-
 // Event listener for the trigger button to show the popup
 triggerButton.addEventListener('click', (e) => {
     e.preventDefault();
     popup.style.display = 'block';
     navigation.style.position = 'static';
-    sessionStorage.setItem('popupShown', 'false');
 });
 
 // Event listener for the close button to hide the popup anc continue in the website
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
     popup.style.display = 'none';
     navigation.style.position = 'sticky';
-    sessionStorage.setItem('popupShown', 'true');
 });
-
-// If the popup has not been shown yet, show it, else hide it
-if (sessionStorage.getItem('popupShown') === 'false') {
-    popup.style.display = 'block';
-    navigation.style.position = 'static';
-} else {
-    const popup = document.getElementById('project-popup');
-    popup.style.display = 'none';
-    navigation.style.position = 'sticky';
-}
